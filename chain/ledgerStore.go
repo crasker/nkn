8182ca59d8f8cab6a6774122f5bce27a3a8d6ce2
crasker <chouyc.adam@gmail.com>
2021-04-21 20:12:34 +0800

    add generate id2 function with registration fee

--- chain/ledgerStore.go
+++ chain/ledgerStore.go
@@ -27,7 +27,9 @@ type ILedgerStore interface {
 	GetSubscribers(topic string, bucket, offset, limit uint32) ([]string, error)
 	GetSubscribersWithMeta(topic string, bucket, offset, limit uint32) (map[string]string, error)
 	GetSubscribersCount(topic string, bucket uint32) int
-	GetID(publicKey []byte) ([]byte, error)
+	GetID(publicKey []byte, height uint32) ([]byte, error)
+	GetID1(publicKey []byte) ([]byte, error)
+	GetID2(publicKey []byte) ([]byte, error)
 	GetBalance(addr common.Uint160) common.Fixed64
 	GetBalanceByAssetID(addr common.Uint160, assetID common.Uint256) common.Fixed64
 	GetNonce(addr common.Uint160) uint64
